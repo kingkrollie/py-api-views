241646cf8aa74ceab7904cdc9994366dea2b31d6
Vadym Vynokur <king.krollie@gmail.com>
2025-12-28 16:58:06 +0200

    add sol

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,41 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
-from cinema.views import movie_list, movie_detail
-
-urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
-]
+from cinema.views import (
+    MovieViewSet,
+    CinemaHallViewSet,
+    ActorList,
+    ActorDetail,
+    GenreList,
+    GenreDetail,
+)
 
 app_name = "cinema"
+
+router = routers.DefaultRouter()
+router.register("movies", MovieViewSet)
+
+cinema_list = CinemaHallViewSet.as_view({
+    "get": "list",
+    "post": "create",
+})
+
+cinema_detail = CinemaHallViewSet.as_view({
+    "get": "retrieve",
+    "put": "update",
+    "patch": "partial_update",
+    "delete": "destroy",
+})
+
+urlpatterns = [
+    path("", include(router.urls)),
+
+    path("cinema_halls/", cinema_list),
+    path("cinema_halls/<int:pk>/", cinema_detail),
+
+    path("actors/", ActorList.as_view()),
+    path("actors/<int:pk>/", ActorDetail.as_view()),
+
+    path("genres/", GenreList.as_view()),
+    path("genres/<int:pk>/", GenreDetail.as_view()),
+]
